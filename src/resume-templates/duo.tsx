fb1bea82b8578688190b667b7ca74d32a0c5fc33
Michael Movsesov <michael.movsesov@gmail.com>
2025-08-20 20:36:59 -0700

    Due template header section

--- src/resume-templates/duo.tsx
+++ src/resume-templates/duo.tsx
@@ -6,6 +6,7 @@ import {
   Document,
   StyleSheet,
   Font,
+  Link,
 } from '@react-pdf/renderer';
 
 Font.register({
@@ -18,10 +19,18 @@ Font.register({
   src: 'https://fonts.gstatic.com/s/poppins/v23/pxiEyp8kv8JHgFVrFJA.ttf',
 });
 
+const colors = {
+  primary: '#dbeafe',
+  secondaryDark: '#3f3f46',
+  secondaryLight: '#a1a1aa',
+};
+
 const styles = StyleSheet.create({
   page: {
-    padding: 50,
-    color: '#3f3f46',
+    padding: 36,
+    color: colors.secondaryDark,
+    fontSize: 10,
+    fontFamily: 'Poppins',
   },
   name: {
     fontFamily: 'Roboto Mono',
@@ -36,9 +45,61 @@ const styles = StyleSheet.create({
     left: 0,
     width: '100%',
     height: 15,
-    backgroundColor: '#dbeafe',
+    backgroundColor: colors.primary,
+  },
+  headingLabel: {
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    color: colors.secondaryLight,
+    marginTop: 2,
+  },
+  contactInfo: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 8,
+    color: colors.secondaryLight,
+  },
+  summary: {
+    marginTop: 18,
+  },
+  sectionTitleWrap: {
+    display: 'flex',
+    position: 'relative',
+    width: 'auto',
+  },
+  sectionTitle: {
+    fontFamily: 'Roboto Mono',
+    fontSize: 16,
+    marginTop: 18,
+  },
+  sectionTitleUnderline: {
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    height: 8,
   },
 });
+
+const SectionTitle = ({
+  title,
+  underlineWidth,
+}: {
+  title: string;
+  underlineWidth: number;
+}) => {
+  return (
+    <View style={styles.sectionTitleWrap}>
+      <View
+        style={{ ...styles.sectionTitleUnderline, width: underlineWidth }}
+      />
+      <Text style={styles.sectionTitle}>{title}</Text>
+    </View>
+  );
+};
 
 const DuoTemplate = ({ resume }: { resume: Resume }) => {
   return (
@@ -46,8 +107,26 @@ const DuoTemplate = ({ resume }: { resume: Resume }) => {
       <Page size="A4" style={styles.page}>
         <View style={styles.headingWrap}>
           <View style={styles.headingUnderline} />
-          <Text style={styles.name}>{resume.basics.name}</Text>
+          <Text style={styles.name}>{resume.basics?.name}</Text>
         </View>
+        <Text style={styles.headingLabel}>
+          {resume.basics?.label?.toUpperCase()}
+        </Text>
+        <View style={styles.contactInfo}>
+          <Text>{resume.basics?.location?.city}</Text>
+          <Text>{resume.basics?.phone}</Text>
+          <Text>{resume.basics?.email}</Text>
+          <Link
+            src={resume.basics?.url}
+            style={{ color: colors.secondaryLight, textDecoration: 'none' }}
+          >
+            {resume.basics?.url}
+          </Link>
+        </View>
+        <View style={styles.summary}>
+          <Text>{resume.basics?.summary}</Text>
+        </View>
+        <SectionTitle title="Work Experience" underlineWidth={150} />
       </Page>
     </Document>
   );
