c89cd04b47665af3442b98a5978be1e064117331
Michael Movsesov <michael.movsesov@gmail.com>
2025-08-21 11:33:34 -0700

    Add download pdf button

--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -1,10 +1,11 @@
 import { FC, useEffect, useState } from 'react';
 
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Grid, GridItem, Icon } from '@chakra-ui/react';
 import { PDFDownloadLink, usePDF } from '@react-pdf/renderer';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
+import { HiDownload } from 'react-icons/hi';
 
 import { useResume } from '../../hooks/useResume';
 import DuoTemplate from '../../resume-templates/duo';
@@ -40,6 +41,32 @@ export const Preview: FC = () => {
       padding="20"
       zIndex={0}
     >
+      <Box
+        as="header"
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        top={0}
+        width="100%"
+        height="60px"
+        bgColor="gray.600"
+        zIndex={900}
+        px={4}
+        boxShadow="md"
+      >
+        <Grid templateColumns="1fr 1fr 1fr" width="100%">
+          <GridItem />
+          <GridItem />
+          <GridItem display="flex" justifyContent="end">
+            <PDFDownloadLink document={template}>
+              <Button size="sm" colorPalette="gray" variant="subtle">
+                <Icon as={HiDownload} boxSize={5} />
+                Download PDF
+              </Button>
+            </PDFDownloadLink>
+          </GridItem>
+        </Grid>
+      </Box>
       <Document
         file={blob}
         onLoadSuccess={onDocumentLoadSuccess}
@@ -52,9 +79,6 @@ export const Preview: FC = () => {
           </Box>
         ))}
       </Document>
-      <PDFDownloadLink document={template}>
-        <Button>Download</Button>
-      </PDFDownloadLink>
     </Box>
   );
 };
