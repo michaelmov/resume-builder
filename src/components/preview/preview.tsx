69502a3876fa639c5cda257c6061ac8b1f45ef01
Michael Movsesov <michael.movsesov@gmail.com>
2025-09-02 09:42:27 -0700

    feat: add zoom in and zoom out functionality to preview

--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -1,7 +1,8 @@
-import { Box, Button } from '@chakra-ui/react';
+import { Box, IconButton, Flex } from '@chakra-ui/react';
 import { usePDF } from '@react-pdf/renderer';
 import { FC, useEffect, useMemo, useState } from 'react';
 import { GoSidebarCollapse, GoSidebarExpand } from 'react-icons/go';
+import { HiOutlineZoomIn, HiOutlineZoomOut } from 'react-icons/hi';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
@@ -13,6 +14,9 @@ import { PreviewNavBar } from './preview-nav-bar';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
+const MAX_SCALE = 2;
+const MIN_SCALE = 0.8;
+
 export const Preview: FC<{
   isEditorCollapsed: boolean;
   onEditorCollapseChange: (isEditorCollapsed: boolean) => void;
@@ -22,6 +26,7 @@ export const Preview: FC<{
   const template = useMemo(() => <DuoTemplate resume={resume} />, [resume]);
   const [instance, update] = usePDF({ document: template });
   const [numPages, setNumPages] = useState<number>();
+  const [scale, setScale] = useState<number>(1.4);
 
   const blob = instance.blob;
 
@@ -46,18 +51,43 @@ export const Preview: FC<{
       zIndex={0}
     >
       <PreviewNavBar resumeTemplate={template} />
-      <Box position="absolute" left={-2}>
-        <Button
+      <Box position="absolute" left={-1}>
+        <IconButton
           onClick={() => onEditorCollapseChange(!isEditorCollapsed)}
           variant="subtle"
+          zIndex="overlay"
         >
           {isEditorCollapsed ? <GoSidebarCollapse /> : <GoSidebarExpand />}
-        </Button>
+        </IconButton>
       </Box>
+      <Flex
+        position="absolute"
+        right={0}
+        bottom={0}
+        direction="column"
+        gap={2}
+        margin={4}
+        zIndex="overlay"
+      >
+        <IconButton
+          onClick={() => setScale(Math.min(scale + 0.1, MAX_SCALE))}
+          variant="subtle"
+          disabled={scale === MAX_SCALE}
+        >
+          <HiOutlineZoomIn />
+        </IconButton>
+        <IconButton
+          onClick={() => setScale(Math.max(scale - 0.1, MIN_SCALE))}
+          variant="subtle"
+          disabled={scale === MIN_SCALE}
+        >
+          <HiOutlineZoomOut />
+        </IconButton>
+      </Flex>
       <Document
         file={blob}
         onLoadSuccess={onDocumentLoadSuccess}
-        scale={1.4}
+        scale={scale}
         className="pdf-document"
       >
         {Array.from({ length: numPages ?? 0 }).map((_, index) => (
